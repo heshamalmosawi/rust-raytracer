e94135529cd9e19edef37170afffdfe8ef014af2
heshamalmosawi <hishamalmosawii@gmail.com>
2025-02-04 15:57:02 +0300

    feat: sphere drawing

--- src/ray.rs
+++ src/ray.rs
@@ -32,8 +32,24 @@ impl Ray {
         `blendedValue` = (1 - t) * startValue + t * endValue
     */
     pub fn get_ray_collor(&self) -> Color {
+        if self.hits_sphere(Point3::new(0.0, 0.0, -1.0), 0.5) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
         let unit_direction = vec3::unit_vector(self.get_direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
+
+    /*
+       This function checks if this ray hits the radius of a given sphere based on the "discriminant".
+       If discriminant is a zero or positive value, it means that the ray hits the sphere, else it does not interact with the sphere.
+    */
+    pub fn hits_sphere(&self, center: Point3, radius: f64) -> bool {
+        let oc = self.get_origin() - center;
+        let a = vec3::dot(self.get_direction(), self.get_direction());
+        let b = 2.0 * vec3::dot(oc, self.get_direction());
+        let c = vec3::dot(oc, oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant >= 0.0
     }
 }
