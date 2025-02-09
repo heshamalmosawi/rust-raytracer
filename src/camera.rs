ef40d9c346e8b743b5bba6a5faabda72b746bf51
heshamalmosawi <hishamalmosawii@gmail.com>
2025-02-09 13:31:00 +0300

    feat: positional camera
    lets goooo

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,10 @@
 use super::util::ASPECT_RATIO;
-use crate::geometry::{
-    ray::Ray,
-    vec3::{Point3, Vec3},
+use crate::{
+    geometry::{
+        ray::Ray,
+        vec3::{self, Point3, Vec3},
+    },
+    util,
 };
 
 pub struct Camera {
@@ -12,18 +15,29 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Self {
-        let viewport_height = 2.0;
-        let viewport_width = ASPECT_RATIO * viewport_height;
-        let focal_length = 1.0; // distance from camera's origin
+    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, field_of_view: f64) -> Self {
+        let theta = util::degrees_to_radians(field_of_view);
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
 
-        let origin = Point3::new(0.0, 0.0, 0.0);
+        let viewport_width = ASPECT_RATIO * viewport_height;
+
+        // three unit vectors:
+        // u: the unit vector pointing to camera right
+        // v: the unit vector pointing to upside direction
+        // w: the unit vector pointing opposite the view direction
+        
+
+        let w = vec3::unit_vector(lookfrom - lookat);
+        let u = vec3::unit_vector(vec3::cross(vup, w));
+        let v = vec3::cross(w, u);
+
+        let origin = lookfrom;
 
         // converting viewports to vectors
-        let height = Vec3::new(0.0, viewport_height, 0.0);
-        let width = Vec3::new(viewport_width, 0.0, 0.0);
-        let lower_left_corner =
-            origin - (width / 2.0) - (height / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+        let height = viewport_height * v;
+        let width = viewport_width * u;
+        let lower_left_corner = origin - width / 2.0 - height / 2.0 - w;
 
         Self {
             origin,
@@ -33,12 +47,12 @@ impl Camera {
         }
     }
 
-    // this function generates a ray frmo the camera, given screen coordinates
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    // this function generates a ray from the camera, given screen coordinates
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
             // bottom left corner with horizontal and vertical offset, minus the origin point of the camera to get the direction vector of the ray
-            self.lower_left_corner + u * self.width + v * self.height - self.origin,
+            self.lower_left_corner + s * self.width + t * self.height - self.origin,
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{io, rc::Rc};
 use rust_raytracer::{
     camera::Camera,
     color::{self, Color},
-    geometry::vec3::Point3,
+    geometry::vec3::{Point3, Vec3},
     hittable::hittable_list::HittableList,
     material::{Dielectric, Lambertian, Metal},
     shapes::sphere::Sphere,
@@ -14,6 +14,8 @@ fn main() {
     const MAX_DEPTH: i32 = 50;
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // let r = (util::PI / 4.0).cos();
     let mut world = HittableList::new();
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
@@ -33,7 +35,12 @@ fn main() {
     )));
     world.add(Box::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
-        -0.4,
+        0.5,
+        material_left.clone(),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.45,
         material_left,
     )));
     world.add(Box::new(Sphere::new(
@@ -42,7 +49,12 @@ fn main() {
         material_right,
     )));
 
-    let cam = Camera::new();
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+    );
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
